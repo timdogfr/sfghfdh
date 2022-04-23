86c7fd1e50db1c8422fd9697beff28a55679d5fc
timdogfr <103558704+timdogfr@users.noreply.github.com>
2022-04-23 20:56:01 +0300

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Image, Box, VStack, Text } from '@chakra-ui/react'
+import logo from '../images/nav_logo.png'
+import NavIcons from './NavIcons'
+
+function Footer() {
+    return (
+        <Box pb={32}>
+            <VStack
+                spacing={12}
+                align="center"
+            >
+                <Image src={logo} h="140px" />
+                <NavIcons />
+
+                <Text>
+                    © 2022 TTC. All right reserved.
+                </Text>
+            </VStack>
+        </Box>
+    )
+}
+
+export default Footer
