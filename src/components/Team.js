a96284721d5805b134041a16ee291db79ada7d14
timdogfr <103558704+timdogfr@users.noreply.github.com>
2022-04-23 20:27:15 +0300

    Update Team.js

--- src/components/Team.js
+++ src/components/Team.js
@@ -32,8 +32,8 @@ function Team() {
             <Box mx={[32, 32, 16, 16, 32]} id="Team">
                 <Center>
                     <SimpleGrid spacing="6vw" columns={["1", "2"]} rowGap={8} >
-                        <TeamMember name="Isim" role="project lead" src={a} />
-                        <TeamMember name="Isim1" role="marketing" src={b} />                   
+                        <TeamMember name="Isim" role="CEO" src={a} />
+                        <TeamMember name="Isim1" role="CEO" src={b} />                   
                     </SimpleGrid>
                 </Center>
             </Box>
