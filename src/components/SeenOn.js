f5cb2727670df8a5e5c88af552d5c57e3ea654e9
timdogfr <103558704+timdogfr@users.noreply.github.com>
2022-04-24 16:50:05 +0300

    Update SeenOn.js

--- src/components/SeenOn.js
+++ src/components/SeenOn.js
@@ -3,10 +3,10 @@ import { Box, VStack, Image, Heading, SimpleGrid, Center, Link } from '@chakra-u
 
 
 
-function Here({ src, name, url }) {
+function Here({ src, name}) {
     return (
         <>
-            <Link w='100%' h='100%' p={4} pb={6} bg='transparent' rounded={16} _hover={{ bgColor: 'rgb(44,44,44, 0.5)' }} _focus={{ outline: 0 }} href={url} target="_blank" rel="noopener noreferrer">
+            <Link w='100%' h='100%' p={4} pb={6} bg='transparent' rounded={16} _hover={{ bgColor: 'rgb(44,44,44, 0.5)' }} _focus={{ outline: 0 }} target="_blank" rel="noopener noreferrer">
                 <Box>
                     <VStack spacing={5}>
                         <Image maxW={64} src={src} />
@@ -30,8 +30,10 @@ function SeenOn() {
             <Box mx={[32, 32, 16, 16, 32]} id="Team">
                 <Center>
                     <SimpleGrid spacing="6vw" columns={["1", "1", "2", "2"]} rowGap={8}>
-                        <Here name="tofunft.com" src={'https://pbs.twimg.com/profile_images/1478611959738408962/ftn_GhBj_400x400.jpg'} url='' />
-                        <Here name="beta.cyberbox.art" src={'https://pbs.twimg.com/profile_images/1496052164816482304/IN1P3BtS_400x400.png'} url='' />
+                        <Here name="tofunft.com" src={'https://pbs.twimg.com/profile_images/1478611959738408962/ftn_GhBj_400x400.jpg'} />
+                        <Here name="beta.cyberbox.art" src={'https://pbs.twimg.com/profile_images/1496052164816482304/IN1P3BtS_400x400.png'} />
+                        <Here name="ariswap.net" src={'https://pbs.twimg.com/profile_images/1490390423285772293/17-PYyfp_400x400.jpg'} />
+                        <Here name="epor.io" src={'https://pbs.twimg.com/profile_images/1358006180413644800/6D30gWMK_400x400.png'} />
                     </SimpleGrid>
                 </Center>
             </Box>
